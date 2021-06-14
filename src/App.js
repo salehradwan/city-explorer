c3c6afd81fc2448cad4f06514d4634e559c4a0b1
salehradwan <saleh-radwan@outlook.com>
2021-06-14 09:59:46 +0300

    add try catch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends PureComponent {
       cityName: '',
       cityData: {},
       displayData: false,
-      hasError: false
+      hasError: ''
 
     }
   };
@@ -15,55 +15,61 @@ export default class App extends PureComponent {
   updateCityState = (evt) => {
     this.setState({
       cityName: evt.target.value,
-
     });
   };
 
   getCityData = async (evt) => {
     evt.preventDefault();
-    const axiosResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.0788dbe4910fd378fe6241b0ac26587d&q=${this.state.cityName}&format=json`);
-    console.log(axiosResponse);
-    this.setState({
-      cityData: axiosResponse.data[0],
-      displayData: true
-    });
-  };
-  componentDidCatch(error, info) {
-    // Display fallback UI
-    this.setState({ 
-      hasError: true 
-    });
-    // You can also log the error to an error reporting service
-    // logErrorToMyService(error, info);
-  }
-  render() {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+    try {
+      const axiosResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.0788dbe4910fd378fe6241b0ac26587d&q=${this.state.cityName}&format=json`);
+      // console.log(axiosResponse);
+      this.setState({
+        cityData: axiosResponse.data[0],
+        displayData: true,
+        alert: false
+      });
+    } catch (error) {
+      this.setState({
+        hasError: error.message,
+        alert: true
+      })
     }
-    return (
-      <div className="App">
-        <form onSubmit={this.getCityData}>
-          <br></br>
-          <br></br>
-          <lable> City Name</lable>
-          <input type='text' onChange={this.updateCityState} />
-          <br></br>
-          <br></br>
-          <input type='submit' value='Explore!' />
-        </form>
-        {
-          this.state.displayData &&
-          <div>
-            <p>{this.state.cityData.display_name}</p>
-            <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.0788dbe4910fd378fe6241b0ac26587d&q&center=${this.state.cityData.lat},
-               ${this.state.cityData.lon}&zoom=10`} alt='' />
 
-          </div>
+  };
+  render() {
+    return (
+      <div>
+        {
+          this.state.alert &&
+          alert(`${this.state.hasError}`)
         }
+        <div className="App">
+          <form onSubmit={this.getCityData}>
+            <br></br>
+            <br></br>
+            <lable> City Name</lable>
+            <input type='text' onChange={this.updateCityState} />
+            <br></br>
+            <br></br>
+            <input type='submit' value='Explore!' />
+          </form>
+          {
+            this.state.displayData &&
+            <div>
+              <p></p>
+              <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.0788dbe4910fd378fe6241b0ac26587d&q&center=${this.state.cityData.lat},
+               ${this.state.cityData.lon}&zoom=10`} alt='' />
+               <p>`The City name is: {this.state.cityData.display_name}`</p>
+               <p>`The lat is: {this.state.cityData.lat}`</p>
+               <p>`The lon is: {this.state.cityData.lon}`</p>
+
+            </div>
+          }
+        </div>
       </div>
+
     );
-    
+
   }
 }
 
