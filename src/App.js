ecabb192b22f754375ca13321e277ccfb7a56cd0
salehradwan <saleh-radwan@outlook.com>
2021-06-15 18:33:01 +0300

    APIs

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ export default class App extends PureComponent {
     this.state = {
       cityName: '',
       cityData: {},
+      lat: '',
+      lon: '',
       weatherData: '',
       displayData: false,
       hasError: ''
@@ -32,8 +34,25 @@ export default class App extends PureComponent {
   getCityData = async (evt) => {
     evt.preventDefault();
     try {
-      const locationIQResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.0788dbe4910fd378fe6241b0ac26587d&q=${this.state.cityName}&format=json`);
-      const weatherResponse = await axios.get(`${process.env.REACT_APP_URL}/weather`);
+      await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.0788dbe4910fd378fe6241b0ac26587d&q=
+      ${this.state.cityName}&format=json`).then(locationIQResponse => {
+        this.setState({
+          cityData: locationIQResponse.data[0],
+          lat: locationIQResponse.data[0].lat,
+          lon: locationIQResponse.data[0].lon,
+        });
+        axios.get(`${process.env.REACT_APP_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}&include=minutely`).then(weatherResponse => {
+          this.setState({
+            weatherData: weatherResponse.data.data,
+            displayData: true,
+            alert: false
+          });
+        });
+      });
+
+
+
+      // const weatherResFromLocall = await axios.get(`${process.env.REACT_APP_PORT}/weather`);
 
       // try {
       //   if (this.state.cityName === 'Seattle' || this.state.cityName === 'Paris' ||
@@ -51,13 +70,6 @@ export default class App extends PureComponent {
       //     alert: true
       //   })
       // }
-      this.setState({
-            cityData: locationIQResponse.data[0],
-            weatherData: weatherResponse.data.data,
-            displayData: true,
-            alert: false
-          });
-      
     } catch (error) {
       this.setState({
         hasError: error.message,
