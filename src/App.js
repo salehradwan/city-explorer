4d7a32222047c2359e55f82590e521c12e81a550
salehradwan <saleh-radwan@outlook.com>
2021-06-16 23:15:52 +0300

    add the movies component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Weather from './components/Weather';
+import Movies from './components/Movies';
+
 
 export default class App extends PureComponent {
   constructor(props) {
@@ -34,21 +36,30 @@ export default class App extends PureComponent {
   getCityData = async (evt) => {
     evt.preventDefault();
     try {
-      await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.0788dbe4910fd378fe6241b0ac26587d&q=
+      await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=
       ${this.state.cityName}&format=json`).then(locationIQResponse => {
         this.setState({
           cityData: locationIQResponse.data[0],
           lat: locationIQResponse.data[0].lat,
           lon: locationIQResponse.data[0].lon,
         });
-        axios.get(`${process.env.REACT_APP_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}&include=minutely`)
-        .then(weatherResponse => {
-          this.setState({
-            weatherData: weatherResponse.data,
-            displayData: true,
-            alert: false
-          })
-        });
+        axios.get(`${process.env.REACT_APP_PORT}/weather?lat=${this.state.lat}&lon=${this.state.lon}&include=minutely`)
+          .then(weatherResponse => {
+            console.log('wewe',weatherResponse);
+            this.setState({
+              weatherData: weatherResponse.data,
+              displayData: true,
+              alert: false
+            })
+          });
+          axios.get(`${process.env.REACT_APP_PORT}/movies?query=${this.state.cityName}`)
+          .then(movieResponse => {
+            this.setState({
+              movieData: movieResponse.data,
+              displayData: true,
+              alert: false
+            })
+          });
       });
 
     } catch (error) {
@@ -86,6 +97,9 @@ export default class App extends PureComponent {
                   />
                   <Weather
                     weatherData={this.state.weatherData}
+                  />
+                  <Movies 
+                    movieData = {this.state.movieData}
                   />
 
                 </>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,15 +3,17 @@ import ListGroup from 'react-bootstrap/ListGroup'
 
 export class Weather extends Component {
     render() {
+        console.log('prp',this.props);
         return (
             <div>
                 {
                     this.props.weatherData.map(value => {
                         return (
                             <>
+        
                                 <ListGroup>
                                     <ListGroup.Item variant="info">Description: {value.description}</ListGroup.Item>
-                                    <ListGroup.Item variant="info">Date: {value.date}</ListGroup.Item>
+                                    <ListGroup.Item variant="info">Date: {value.datetime}</ListGroup.Item>
                                    </ListGroup>
                             </>
                         )
